0bc7d6721a41d3bb5032a6006b48dbf54a0fc24f
aam <aam@anahoret.com>
2018-04-05 17:46:21 +0300

    Send Bad credential response when POST /auth request body is empty or credentials are actually bad.

--- src/main/kotlin/com/anahoret/jwtlearn/controller/AuthenticationController.kt
+++ src/main/kotlin/com/anahoret/jwtlearn/controller/AuthenticationController.kt
@@ -1,13 +1,16 @@
 package com.anahoret.jwtlearn.controller
 
 import com.anahoret.jwtlearn.security.JwtTokenUtil
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.userdetails.UserDetailsService
 
 @Controller
 class AuthenticationController(
@@ -16,18 +19,25 @@ class AuthenticationController(
     private val jwtTokenUtil: JwtTokenUtil
 ) {
 
+    private val logger = LoggerFactory.getLogger(AuthenticationController::class.java)
+
     @PostMapping("auth")
-    fun authenticate(@RequestBody authenticationRequest: AuthenticationRequest): ResponseEntity<*> {
-        authenticationManager.authenticate(UsernamePasswordAuthenticationToken(
-            authenticationRequest.username, authenticationRequest.password))
-
-        val userDetails = userDetailsService.loadUserByUsername(authenticationRequest.username);
-        val token = jwtTokenUtil.createToken(userDetails);
-
-        return ResponseEntity.ok(JwtAuthenticationResponse(token));
+    fun authenticate(@RequestBody authenticationRequest: AuthenticationRequest?): ResponseEntity<*> {
+        return authenticationRequest?.let { (username, password) ->
+            try {
+                authenticationManager.authenticate(UsernamePasswordAuthenticationToken(username, password))
+                val userDetails = userDetailsService.loadUserByUsername(username)
+                val token = jwtTokenUtil.createToken(userDetails);
+                ResponseEntity.ok(JwtAuthenticationResponse(token));
+            } catch (e: AuthenticationException) {
+                logger.debug("Authentication error", e)
+                null
+            }
+        } ?: ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(AuthenticationError("Bad credentials"))
     }
 
-    class AuthenticationRequest(val username: String, val password: String)
+    data class AuthenticationRequest(val username: String? = null, val password: String? = null)
+    class AuthenticationError(val error: String)
     class JwtAuthenticationResponse(val token: String)
 
 }
